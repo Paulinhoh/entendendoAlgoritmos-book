51e77aad549b2e40691b36fc0a4089b299b4c925
Paulo Henrique Reis <phdsreis@gmail.com>
2025-11-25 01:43:49 -0300

    refactor files

--- exercicios/capitulo-01/1.6/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-/*
-	Forneça o tempo de execução para cada um dos casos a seguir em temos de notação Big O.
-
-	1.6 -> .Você deseja ler os numeros apenas dos nomes que começam com A
-
-	r: O(n)
-*/
